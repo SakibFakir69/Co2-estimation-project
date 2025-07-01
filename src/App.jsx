3a2d84d9ff8e1003f5382cdb11bdb7766f40091f
Sakib Fakir <FakirSakib22232@gmail.com>
2025-07-01 23:47:54 +0600

    : map add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { MapContainer,TileLayer,Popup,Marker } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 
 function App() {
   // co2
 
- 
-
   return (
-    <div>
-      <h1>map</h1>
+    <div className="flex flex-col w-full justify-center items-center">
+      <h1 className="py-10 mt-10">map</h1>
       
+
+      <MapContainer center={[23.8103, 90.4125]} style={{ height: "400px", width: "100%", maxWidth: "600px" }} zoom={13} scrollWheelZoom={true} className="max-w-lg py-20">
+        <TileLayer
+          
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[23.8103, 90.4125]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
